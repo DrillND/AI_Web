8a4f3e635165ab841ea57d67df10c32f3ebdae8a
DrillND <gornhub13@gmail.com>
2023-08-16 17:42:03 +0900

    1

--- web/app.js
+++ web/app.js
@@ -1,16 +1,49 @@
 const express = require('express');
 const app = express();
 
+const passport          = require('passport');
+const passportConfig    = require('./passport');
+const session           = require('express-session');
+const flash             = require('connect-flash');
+
+passportConfig()
+
+//세션 설정
+app.use(
+    session({
+       resave: false,
+       saveUninitialized: false,
+       secret: "sessionsecretsessionsecret",
+    }),
+);
+//passport 초기화
+app.use(passport.initialize()); //req에 passport 설정 추가
+app.use(passport.session());    //req.session에 passport 데이터 추가
+app.use(flash());
+
+app.use(express.static(__dirname + '/public'));
+
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 //라우트 객체 생성
 const mainRouter = require('./routes/index');
 const dustRouter    = require('./routes/dust');
+const userRouter    = require('./routes/user');
+const newsRouter    = require('./routes/news');
+const wifiRouter = require('./routes/wifi');
+const maskRouter    = require('./routes/mask');
+
 //라우트 설정
 app.use('/', mainRouter);
 app.use('/dust', dustRouter);
+app.use('/user', userRouter);
+app.use('/news', newsRouter);
+app.use('/wifi', wifiRouter);
+app.use('/mask', maskRouter);
 
 const PORT = 8080;
 app.listen(PORT, function() { //무한대기.. 요청들어오면 쓰레드를 던진다.
--- web/routes/index.js
+++ web/routes/index.js
@@ -1,7 +1,8 @@
 const express     = require('express');
 const router      = express.Router();
+const auth        = require('./auth');
 
-router.get('/', function(req, res) {
+router.get('/', auth.CheckAuth, function(req, res) {
     //res.send('라우트를 사용한 메인 페이지입니다.');
     res.render('index');
 });
